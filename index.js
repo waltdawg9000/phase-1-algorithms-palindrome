509d277a49ffcb9a34d49a727b6597cb8e4b9dcc
Walter Morris <waltercmorris18@gmail.com>
2022-01-03 23:23:03 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
 }
-
+function isPalindrome(word) {
+  if (word.length <= 1) {
+    return true
+  } else {
+    if(word[0] === word[word.length - 1]) {
+      
+      return true
+    } else {
+      return false
+    }
+  }
+}
 /* 
   Add your pseudocode here
 */
